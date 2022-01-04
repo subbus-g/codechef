3b67d703fa686bb81b80fe14d5f73e2f10611827
subbus <subbareddyg83@gmail.com>
2022-01-04 12:36:19 +0530

    refactor zco14003

--- LRNDSA01--DSA Learning Series - Contest 1/ZCO14003-Smart Phone.cpp
+++ LRNDSA01--DSA Learning Series - Contest 1/ZCO14003-Smart Phone.cpp
@@ -1,4 +1,5 @@
 // https://www.codechef.com/LRNDSA01/problems/ZCO14003
+// https://github.com/subbus-g/codechef/blob/4533209b536150673475f2796d9cbaebb3331e76/LRNDSA01--DSA%20Learning%20Series%20-%20Contest%201/ZCO14003-Smart%20Phone.cpp
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
@@ -13,23 +14,21 @@ int main()
 
     /*******************code starts from here********************************/
 
-    long long n;
+    int n;
     cin >> n;
-    vector<long long> budgets(n);
+    vector<int> budgets(n);
     for (auto &budget : budgets)
     {
         cin >> budget;
     }
+    //sort the budgets in desceding order
     sort(budgets.begin(), budgets.end(), std::greater<>());
 
-    long max_revenue = 0;
+    long long max_revenue = 0;
     for (int i = 0; i < n; i++)
     {
-        long current_revenue = budgets[i] * (i + 1);
-        if (current_revenue > max_revenue)
-        {
-            max_revenue = current_revenue;
-        }
+        //budget of a given customer as fixed price * no of customers whose budget is less than or equal to given customer budget gives the revenue
+        max_revenue = max(max_revenue, (long long)budgets[i] * (i + 1));
     }
     cout << max_revenue;
     return 0;
